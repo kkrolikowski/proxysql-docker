ecf6338227d0b0eaf167c7b19d1fba078c7a9d2f
Krzysztof Królikowski <kkrolikowski@gmail.com>
2019-05-12 21:41:53 +0200

    pass sqlproxy admin credential as variables

--- proxysql-entry.sh
+++ proxysql-entry.sh
@@ -9,4 +9,15 @@ if [ -z "$DISCOVERY_SERVICE" ]; then
         exit 1
 fi
 
+if [ -z "$PROXY_ADMIN_USER" ]; then
+        echo >&2 'Error:  You need to specify PROXY_ADMIN_USER'
+        exit 1
+fi
+
+if [ -z "$PROXY_ADMIN_PASS" ]; then
+        echo >&2 'Error:  You need to specify PROXY_ADMIN_PASS'
+        exit 1
+fi
+
+sed -i "s/admin_credentials=\"admin:admin\"/admin_credentials=\"$PROXY_ADMIN_USER:$PROXY_ADMIN_PASS\"/" /etc/proxysql.cnf
 /usr/bin/proxysql --initial -f -c /etc/proxysql.cnf 
